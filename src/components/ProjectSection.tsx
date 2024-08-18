0c606300e64e4d6203cada868e62c9e0b3701d9f
William-Dantas-Dev <shiroe.gaming.dev@gmail.com>
2024-08-18 03:22:25 -0300

    new projects

--- src/components/ProjectSection.tsx
+++ src/components/ProjectSection.tsx
@@ -14,7 +14,7 @@ const projectsData = [
     previewUrl: "https://photos-gallery-weld.vercel.app/",
   },
   {
-    id: 1,
+    id: 2,
     title: "Passcode",
     description: "The site is a demo of how to password protect pages in your application using a passcode. The password is '123456'. When you visit the site, you will be prompted to enter the password. If you enter the correct password, you will be able to access the site. Otherwise, you will be denied access.",
     image: "/images/projects/2.png",
@@ -23,7 +23,7 @@ const projectsData = [
     previewUrl: "https://passcode-next-js.vercel.app",
   },
   {
-    id: 1,
+    id: 3,
     title: "Hotel Reservation",
     description: "This is an application made in next.js for study. a hotel booking landing page",
     image: "/images/projects/3.png",
@@ -49,6 +49,51 @@ const projectsData = [
     gitUrl: "https://github.com/William-Dantas-Dev/dating-request-nextjs",
     previewUrl: "https://dating-request-mu.vercel.app",
   },
+  {
+    id: 6,
+    title: "Parkour Game",
+    description: "Simple Parkour System make with Unity",
+    image: "/images/projects/6.png",
+    tag: ["All", "Games"],
+    gitUrl: "https://github.com/William-Dantas-Dev/ParkourGame/tree/master",
+    previewUrl: "https://william-shiroe-dev.itch.io/parkour-game",
+  },
+  {
+    id: 7,
+    title: "Roll A Ball",
+    description: "Simple game made with Unity with the objective to collect all cubes.",
+    image: "/images/projects/7.png",
+    tag: ["All", "Games"],
+    gitUrl: "https://github.com/William-Dantas-Dev/RollABall",
+    previewUrl: "https://william-shiroe-dev.itch.io/roll-a-ball",
+  },
+  {
+    id: 8,
+    title: "Procedural Maze Game",
+    description: "Game made with Unreal Engine featuring a procedurally generated maze.",
+    image: "/images/projects/8.png",
+    tag: ["All", "Games"],
+    gitUrl: "https://william-shiroe-dev.itch.io/maze",
+    previewUrl: "https://github.com/William-Dantas-Dev/MazeGame",
+  },
+  {
+    id: 9,
+    title: "Zombie Survival",
+    description: "Project Zombie is a simple survival game made with Unreal Engine. Fight off endless waves of zombies and see how long you can last!",
+    image: "/images/projects/9.png",
+    tag: ["All", "Games"],
+    gitUrl: "",
+    previewUrl: "https://william-shiroe-dev.itch.io/project-zombie",
+  },
+  {
+    id: 10,
+    title: "Stacks Blocks",
+    description: "Stack Blocks is an endless stacking game where you try to stack as many blocks as possible without letting them fall. How high can you go? Challenge yourself and see how many blocks you can stack!",
+    image: "/images/projects/10.png",
+    tag: ["All", "Games"],
+    gitUrl: "https://github.com/William-Dantas-Dev/Unity-StacksBlocks",
+    previewUrl: "https://william-shiroe-dev.itch.io/stackblocks",
+  },
 ];
 
 const ProjectSection = () => {
@@ -66,7 +111,6 @@ const ProjectSection = () => {
       <div className='text-white flex flex-row justify-center items-center gap-2 py-6'>
         <ProjectTag name='All' isSelected={tag === "All"} onClick={handleTagChange}/>
         <ProjectTag name='Web' isSelected={tag === "Web"} onClick={handleTagChange}/>
-        {/* <ProjectTag name='Mobile' isSelected={tag === "Mobile"} onClick={handleTagChange}/> */}
         <ProjectTag name='Games' isSelected={tag === "Games"} onClick={handleTagChange}/>
       </div>
       <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 md:gap-4'>
